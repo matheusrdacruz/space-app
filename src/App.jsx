e20124ec242246522024fc255e4af3002126600d
matheuscruz <matheuscruz@touchtec.com.br>
2024-05-24 23:17:33 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
 
 const GradientBackground = styled.div`
-  background: linear-gradient(100.61deg, #153a69 4.16%, #255aa0 48%, #4174b3 96.76%);
+  background: linear-gradient(100.61deg, #153a69 4.16%, #a9afb8 48%, #cfd7e0 96.76%);
   width: 100%;
   min-height: 100vh;
 `
